968670c31bb2b2a896f3db5fb915d325a6419aa7
nehaprakriya <81734507+nehaprakriya@users.noreply.github.com>
2023-08-18 14:13:33 -0700

    Update split_kernel.py

--- backend/python/tapa/split_kernel.py
+++ backend/python/tapa/split_kernel.py
@@ -8,7 +8,17 @@ from typing import Dict, List
 _logger = logging.getLogger().getChild(__name__)
 
 def sanitize_names(name:str):
-    return name.replace('TASK_VERTEX_', '')
+    # return name.replace('TASK_VERTEX_', '')
+    if ' ' in name and '[' not in name:
+        return name.replace(' ', '')
+    elif '[' in name:
+        name = str(name.split('[')[0])
+        if ' ' in name:
+            name = name.replace(' ', '')
+            _logger.info(name)
+        return name
+    else:
+        return name
 
 def add_tasks(input_file:str, file1, file2, program:tapa.core.Program):
     with open(input_file, 'r') as top:
@@ -44,6 +54,8 @@ def add_ports(input_file:str, file1, file2, device_1_items, device_2_items, prog
             file1.write('tapa::mmap<'+ctype[:-1]+' '+port.name+',\n')
         elif port.name+'__m_axi' in device_2_names and str(port.cat).split('.')[1]=='MMAP':
             file2.write('tapa::mmap<'+ctype[:-1]+' '+port.name+',\n')
+    file1.write(')\n')
+    file2.write(')\n')
     
 # def add_inter_fpga_comm(input_file:str, file1, file2, device_1_items, device_2_items, program:tapa.core.Program):
 #     # get which ones are being sent out of the board and which are being sent into the board and write those into the args
@@ -70,20 +82,61 @@ def find_partitions(device_1_items, device_2_items):
         out_edges = vertex.out_edges
         for in_edge in in_edges:
             if in_edge.src.name in device_2_names:
-                partition_1_in.append(vertex.name)
+                partition_1_in.append((vertex.name, in_edge.name))
         for out_edge in out_edges:
             if out_edge.dst.name in device_2_names:
-                partition_1_out.append(vertex.name)
+                partition_1_out.append((vertex.name, out_edge.name))
     for vertex in device_2_items:
         in_edges = vertex.in_edges
         out_edges = vertex.out_edges
         for in_edge in in_edges:
             if in_edge.src.name in device_1_names:
-                partition_2_in.append(vertex.name)
+                partition_2_in.append((vertex.name, in_edge.name))
         for out_edge in out_edges:
             if out_edge.dst.name in device_1_names:
-                partition_2_out.append(vertex.name)
+                partition_2_out.append((vertex.name, out_edge.name))
     return partition_1_in, partition_1_out, partition_2_in, partition_2_out
+
+def add_invokes(input_file:str, file1, file2, device_1_items, device_2_items, program:tapa.core.Program):
+    file_input = open(input_file, 'r')
+    lines = file_input.readlines()
+    device_1_names = []
+    device_2_names = []
+    for vertex in device_1_items:
+        device_1_names.append(vertex.name)
+    for vertex in device_2_items:
+        device_2_names.append(vertex.name)
+    device_1_names = '\t'.join(device_1_names)
+    device_2_names = '\t'.join(device_2_names)
+    middle_streams = []
+    for line in lines:
+        if 'tapa::stream' in line:
+            middle_streams.append(str(line.split('>')[1])[:-2])
+            file1.write(line)
+            file2.write(line)
+    middle_streams = '\t'.join(middle_streams)
+    _logger.info(middle_streams)
+    for line in lines:
+        if '.invoke' in line:
+            task_name = str(line.split('(')[1]).split(',')[0]
+            edge_1_name = str(line.split('(')[1]).split(',')[1]
+            _logger.info(task_name)
+            _logger.info(edge_1_name)
+            task_name = sanitize_names(task_name)
+            edge_1_name = sanitize_names(edge_1_name)
+            _logger.info(edge_1_name)
+            _logger.info(type(edge_1_name))
+            if 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_1_names:
+                _logger.info("hello")
+            if task_name in device_1_names and 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_1_names or task_name in device_1_names and edge_1_name in middle_streams:
+                file1.write(line)
+                _logger.info(task_name)
+                device_1_names = device_1_names.replace('TASK_VERTEX_'+task_name, '',1)
+                _logger.info(device_1_names)
+            elif task_name in device_2_names and 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_2_names or task_name in device_2_names and edge_1_name in middle_streams:
+                file2.write(line)
+                device_2_names = device_2_names.replace('TASK_VERTEX_'+task_name, '',1)
+
 
 
 def split_kernel(v2s:Dict[Vertex, Slot], slot_list:List[Slot], program:tapa.core.Program, input_file):
@@ -108,181 +161,5 @@ def split_kernel(v2s:Dict[Vertex, Slot], slot_list:List[Slot], program:tapa.core
     _logger.info(partition_1_out)
     _logger.info(partition_2_in)
     _logger.info(partition_2_out)
+    add_invokes(input_file, file1, file2, device_1_items, device_2_items, program)
     # add_inter_fpga_comm(input_file, file1, file2, device_1_items, device_2_items, program)
-
-def split_kernel_1(v2s:Dict[Vertex, Slot] , slot_list:List[Slot], program:tapa.core.Program, input_file):
-    _logger.info("slot list from split kernel")
-    # _logger.info(slot_list)
-    for slots in slot_list:
-        _logger.info(slots.getName())
-    _logger.info(program.top)
-    _logger.info(program.tasks)
-    _logger.info("getting program cpp dir")
-    _logger.info(program.cpp_dir)
-    tasks = []
-    for task in program.tasks:
-        _logger.info(program.get_cpp(task.name))
-        _logger.info(task.name)
-        tasks.append(task.name)
-    # device 1: X0Y0-X7Y7
-    # device 2: X0Y7-X7Y15
-    device_1 = []
-    # device_1_args = []
-    device_2 = []
-    for vertex in v2s:
-        _logger.info(vertex.name)
-        _logger.info(v2s[vertex].getName())
-        _logger.info("partition to")
-        # if int(v2s[vertex].getName()[15])
-        if int(v2s[vertex].getName()[15])<=7:
-            # if 'TASK_VERTEX' in vertex.name:
-            # sanitized_name = vertex.name[12:]
-            # sanitized_name = sanitized_name[:2]
-            device_1.append(vertex.name[12:])
-            # _logger.info(len(vertex.in_edges))
-            if len(vertex.in_edges)>0:
-                for i in range(len(vertex.in_edges)):
-                    _logger.info(vertex.in_edges[i].name)
-            _logger.info("added %s to device 1", vertex.name)
-        else:
-            # if 'TASK_VERTEX' in vertex.name:
-            device_2.append(vertex.name[12:])
-            if len(vertex.in_edges)>0:
-                for i in range(len(vertex.in_edges)):
-                    _logger.info(vertex.in_edges[i].name)
-            # _logger.info(vertex.in_edges[0].name)
-            _logger.info("added %s to device 2", vertex.name)
-    for port in program.toplevel_ports:
-        _logger.info(port.cat)
-        _logger.info(port.name)
-        _logger.info(port.width)
-        _logger.info(port.ctype)
-    file1 = open('kernel_1.cpp', 'w+')
-    file2 = open('kernel_2.cpp', 'w+')
-    with open(input_file, 'r') as top:
-        lines = top.readlines()
-        for line in lines:
-            if 'void '+program.top not in line:
-                file1.write(line)
-                file2.write(line)
-            else:
-                break
-
-    file1.write('void '+program.top+'_1(')
-    file2.write('void '+program.top+'_2(')
-    numports = len(program.toplevel_ports)
-    count=1
-    device_1_items = '\t'.join(device_1)
-    device_2_items = '\t'.join(device_2)
-    _logger.info("device_1_items")
-    _logger.info(device_1_items)
-    _logger.info("device_2_items")
-    _logger.info(device_2_items)
-    for port in program.toplevel_ports:
-        cat_type = str(port.cat).split('.')[1]
-        width = port.width
-        name = port.name
-        _logger.info(name)
-        ctype = port.ctype
-        if cat_type=='MMAP':
-            if port.name+'__m_axi' in device_1_items:
-                _logger.info(port.name)
-                toadd = 'tapa::mmap<INTERFACE_WIDTH> '+name+',\n'
-                file1.write(toadd)
-            elif port.name+'__m_axi' in device_2_items:
-                toadd = 'tapa::mmap<INTERFACE_WIDTH> '+name+',\n'
-                file2.write(toadd)
-        elif cat_type=='SCALAR':
-            toadd = ctype+' '+name+',\n'
-            file1.write(toadd)
-            file2.write(toadd)
-        # elif count==numports:
-    file1.write('tapa::ostream<float> &out_board,\n')
-    file1.write('tapa::istream<float> &in_board){\n')
-    file2.write('tapa::ostream<float> &out_board,\n')
-    file2.write('tapa::istream<float> &in_board){\n')
-    _logger.info(device_1_items)
-    _logger.info(device_2_items)
-    for elem in device_1:
-        if 'axi' in elem or 'external_controller' in elem:
-            device_1.remove(elem)
-    for elem in device_2:
-        if 'axi' in elem or 'external_controller' in elem:
-            device_2.remove(elem)
-    device_1_items = '\t'.join(device_1)
-    device_2_items = '\t'.join(device_2)
-    _logger.info(device_1_items)
-    _logger.info(device_2_items)
-    # TODO need something for the strweams in between the args and middle
-
-
-    with open(input_file, 'r') as top:
-        lines = top.readlines()
-        for line in lines:
-            if 'tapa::stream' in line:
-                file1.write(line)
-                file2.write(line)
-
-    file1.write('tapa::task()\n')
-    file2.write('tapa::task()\n')
-    with open(input_file, 'r') as top:
-        lines = top.readlines()
-        for line in lines:
-            if '.invoke' in line:
-                line = line.split('(')
-                line = line[1].split(',')
-                _logger.info(line)
-                if line[0][1:] in device_1_items:
-                    # device_1_list = device_1_items.split(line[0])
-                    device_1_items = device_1_items.replace(line[0], "", 1)
-                    # device_1_items =''
-                    # for elem in device_1_list:
-                    #     device_1_items+=elem
-                    _logger.info(line[0])
-                    _logger.info(device_1_items)
-                    to_write = '.invoke('
-                    for elem in line:
-                        if ')' in elem:
-                            to_write+=elem
-                        else:
-                            to_write+=elem+', '  
-                    file1.write(to_write)
-                elif line[0][1:] in device_2_items:
-                    device_2_items = device_2_items.replace(line[0], "", 1)
-                    to_write = '.invoke('
-                    for elem in line:
-                        if ')' in elem:
-                            to_write+=elem
-                        else:
-                            to_write+=elem+', '  
-                    file2.write(to_write)
-    file1.write(';\n')
-    file1.write('}')
-    file2.write(';\n')
-    file2.write('}')
-
-                    # file1.write()
-                # file1.write(line)
-
-    # for vertex in v2s:
-    #     if vertex.name in device_1_items and 'TASK_VERTEX' in vertex.name:
-    #         _logger.info(vertex.name)
-    #         _logger.info(vertex.in_edge_names)
-    #         in_edges = vertex.in_edge_names
-    #         in_edges_sanitized = []
-    #         for edge in in_edges:
-    #             edge = edge[10:]
-    #             in_edges_sanitized.append(edge)
-    #         out_edges_sanitized=[]
-    #         out_edges = vertex.out_edge_names
-    #         for edge in out_edges:
-    #             edge = edge[10:]
-    #             out_edges_sanitized.append(edge)
-    #         _logger.info(','.join(in_edges_sanitized))
-    #         _logger.info(','.join(out_edges_sanitized))
-            
-            # file1.write('\t.invoke('+vertex.name, )
-            # file1.write('\t.invoke('+vertex.name+'\n')
-
-
-
