2cb755bd0fb2c4cf53efaa862421fc29810b8505
nehaprakriya <81734507+nehaprakriya@users.noreply.github.com>
2023-08-19 15:47:53 -0700

    Update split_kernel.py

--- backend/python/tapa/split_kernel.py
+++ backend/python/tapa/split_kernel.py
@@ -1,3 +1,5 @@
+# problem with the subscript matching and edges: basically wrong functions are going to the wrong file
+
 from tapa.AutoBridge.src.autobridge.Opt.DataflowGraph import Edge, Vertex
 from tapa.AutoBridge.src.autobridge.Opt.Slot import Slot
 import tapa.core
@@ -51,11 +53,13 @@ def add_ports(input_file:str, file1, file2, device_1_items, device_2_items, prog
         ctype = port.ctype
         # we need to write tapa::mmap<ctype> port.name
         if port.name+'__m_axi' in device_1_names and str(port.cat).split('.')[1]=='MMAP':
-            file1.write('tapa::mmap<'+ctype[:-1]+' '+port.name+',\n')
+            file1.write('tapa::mmap<'+ctype[:-1]+'> '+port.name+',\n')
         elif port.name+'__m_axi' in device_2_names and str(port.cat).split('.')[1]=='MMAP':
-            file2.write('tapa::mmap<'+ctype[:-1]+' '+port.name+',\n')
+            file2.write('tapa::mmap<'+ctype[:-1]+'> '+port.name+',\n')
     file1.write(')\n')
     file2.write(')\n')
+    file1.write('{\n')
+    file2.write('{\n')
     
 # def add_inter_fpga_comm(input_file:str, file1, file2, device_1_items, device_2_items, program:tapa.core.Program):
 #     # get which ones are being sent out of the board and which are being sent into the board and write those into the args
@@ -97,6 +101,32 @@ def find_partitions(device_1_items, device_2_items):
                 partition_2_out.append((vertex.name, out_edge.name))
     return partition_1_in, partition_1_out, partition_2_in, partition_2_out
 
+def unique_task_counts(input_file:str):
+    file_input = open(input_file, 'r')
+    lines = file_input.readlines()
+    unique_tasks = []
+    task_counts = []
+    for line in lines:
+        if '.invoke' in line:
+            task_name = str(line.split('(')[1]).split(',')[0]
+            task_name = sanitize_names(task_name)
+            if task_name not in unique_tasks:
+                unique_tasks.append(task_name)
+                task_counts.append(0)
+
+    for line in lines:
+        if '.invoke' in line:
+            task_name = str(line.split('(')[1]).split(',')[0]
+            task_name = sanitize_names(task_name)
+            for i in range(len(unique_tasks)):
+                if unique_tasks[i]==task_name:
+                    task_counts[i]+=1
+    return unique_tasks, task_counts
+
+    
+        
+
+
 def add_invokes(input_file:str, file1, file2, device_1_items, device_2_items, program:tapa.core.Program):
     file_input = open(input_file, 'r')
     lines = file_input.readlines()
@@ -116,6 +146,8 @@ def add_invokes(input_file:str, file1, file2, device_1_items, device_2_items, pr
             file2.write(line)
     middle_streams = '\t'.join(middle_streams)
     _logger.info(middle_streams)
+    unique_tasks, task_counts = unique_task_counts(input_file)
+    already_invoked = []
     for line in lines:
         if '.invoke' in line:
             task_name = str(line.split('(')[1]).split(',')[0]
@@ -124,18 +156,20 @@ def add_invokes(input_file:str, file1, file2, device_1_items, device_2_items, pr
             _logger.info(edge_1_name)
             task_name = sanitize_names(task_name)
             edge_1_name = sanitize_names(edge_1_name)
-            _logger.info(edge_1_name)
-            _logger.info(type(edge_1_name))
-            if 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_1_names:
-                _logger.info("hello")
-            if task_name in device_1_names and 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_1_names or task_name in device_1_names and edge_1_name in middle_streams:
+            counts = 0
+            for i in range(len(already_invoked)):
+                if already_invoked[i]==task_name:
+                    counts+=1
+            _logger.info(counts)
+            already_invoked.append(task_name)
+            if task_name+'_'+str(counts) in device_1_names:
                 file1.write(line)
                 _logger.info(task_name)
-                device_1_names = device_1_names.replace('TASK_VERTEX_'+task_name, '',1)
+                device_1_names = device_1_names.replace('TASK_VERTEX_'+task_name+'_'+str(counts), '')
                 _logger.info(device_1_names)
-            elif task_name in device_2_names and 'ASYNC_MMAP_VERTEX_'+edge_1_name+'__m_axi' in device_2_names or task_name in device_2_names and edge_1_name in middle_streams:
+            elif task_name+'_'+str(counts) in device_2_names:
                 file2.write(line)
-                device_2_names = device_2_names.replace('TASK_VERTEX_'+task_name, '',1)
+                device_2_names = device_2_names.replace('TASK_VERTEX_'+task_name+'_'+str(counts), '')
 
 
 
@@ -162,4 +196,6 @@ def split_kernel(v2s:Dict[Vertex, Slot], slot_list:List[Slot], program:tapa.core
     _logger.info(partition_2_in)
     _logger.info(partition_2_out)
     add_invokes(input_file, file1, file2, device_1_items, device_2_items, program)
+    file1.write('}\n')
+    file2.write('}\n')
     # add_inter_fpga_comm(input_file, file1, file2, device_1_items, device_2_items, program)
